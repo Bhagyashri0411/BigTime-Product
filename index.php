19cdd7f9f621b586e01496314e00de97d0c6fc19
Bhagyashri0411 <bhagyashripawar0411@gmail.com>
2022-09-02 22:36:53 +0530

    done My work

--- index.php
+++ index.php
@@ -18,6 +18,27 @@
  
 
 </head>
+<style>
+  button svg{
+    width: 16px;
+  fill: white;
+  }
+  .mybtn {
+  display: none;  
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+    z-index: 99;
+    width: auto;
+    border-radius: 50%;
+    border: none;
+    box-shadow: 0px 4px 33px 0px rgb(0 0 86 / 28%);
+    outline: none;
+    background-color: rgb(0, 32, 75);
+    cursor: pointer;
+    padding: 15px 20px;
+}
+</style>
 
 <body>
 
@@ -31,9 +52,31 @@
     <div id="client"></div>
     <div id="Footer"></div>
 </div>
+<button onclick="topFunction()" id="myBtn" class="mybtn" title="Go to top"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg> </button>
 
 <!-- <div id="showme">hiii</div> -->
 
 <script src="./js/home.js"></script>
+<script>
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
 </body>
 </html>
