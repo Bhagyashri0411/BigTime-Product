3e2e41e4853655912b220f24fc3be078b326c543
Bhagyashri0411 <bhagyashripawar0411@gmail.com>
2022-10-07 22:48:59 +0530

    done

--- Pages/home.php
+++ Pages/home.php
@@ -73,7 +73,7 @@
 
 <!-- ***************************SERVICE SECTION****************************** -->
 
-<section class="home-packages">
+<section class="home-packages" id='ser'>
    <div class="mainheading">
       <h2> services</h2>
    </div>
@@ -137,6 +137,121 @@
 
    </div>
 </section>
+
+<!-- ***************************SERVICES SECTION****************************** -->
+
+
+<section class="capabilities" data-scrollreveal="move 0px after 0s over 2s">
+
+<div class="container-fluid">
+
+   <div class="row">
+
+      <div class="col-md-5 cabtab-wrap">
+
+         <h2 class="tab-title">Services</h2>
+         <nav>
+            <ul class="nav nav-tabs" id="pills-tab" role="tablist" style="display:block">
+               <li class="nav-item" role="presentation">
+                  <a class="nav-link ftab0 active" id="pills-home-tab" data-bs-toggle="pill"
+                     data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
+                     aria-selected="true"><span>Hire Remote Developer</span></a>
+               </li>
+               <li class="nav-item" role="presentation">
+                  <a class="nav-link ftab0" id="pills-profile-tab" data-bs-toggle="pill"
+                     data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile"
+                     aria-selected="true"><span>Softwere Development</span></a>
+
+               </li>
+               <li class="nav-item" role="presentation">
+                  <a class="nav-link ftab0 " id="pills-contact-tab" data-bs-toggle="pill"
+                     data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact"
+                     aria-selected="true"><span>Data Entry and Admin</span></a>
+               </li>
+            </ul>
+         </nav>
+
+      </div>
+
+      <div class="col-md-7 align-self-center tbcontent-wrap">
+         <div class="tab-content" id="pills-tabContent">
+
+            <div class="tab-pane fade fcontent0 active show" id="pills-home" role="tabpanel"
+               aria-labelledby="pills-home-tab">
+               <div class="overlay">
+                  <h3 class="main-head">HIRE DEDICATED DEVELOPER MODEL</h3>
+                  <p>Hiring remote developers enables firms to quickly scale up with relatively little risk and
+                     overhead costs. You can alleviate skill gaps by hiring specialists with a direct knowledge in
+                     technology that offers using remote developers.</p>
+                  <p>The duties of a normal software developer are carried out by a remote software developer as
+                     well. The only prominent difference is that remote software developers are separated from the
+                     rest of their team by distance. The creation of new native or hybrid mobile apps, signify web
+                     development, theme customization/development, plugin/extension development, and web or mobile
+                     app development are all jobs that a highly motivated team of developers can handle.</p>
+                     <!-- <div class="about-btn">
+                     <a href="about.php" class="learn-btn">Learn More</a>
+                  </div> -->
+               </div>
+            </div>
+            <div class="tab-pane fade fcontent0" id="pills-profile" role="tabpanel"
+               aria-labelledby="pills-profile-tab">
+               <div class="overlay">
+                  <h3 class="main-head">SOFTWARE DEVELOPMENT</h3>
+
+
+                  <p>The method programmers use to create software applications is called software development.
+                     The procedure, commonly referred to as the Software Development Life Cycle (SDLC), consists
+                     of a number of stages that offer a way to create products that adhere to both technical
+                     requirements and user requirements.</p>
+                  <p>Software developers can use the SDLC as a global standard while creating and enhancing their
+                     software applications. It provides a clear framework that development teams may adhere to
+                     when designing, producing, and maintaining high-quality software. The goal of the process for
+                     creating IT software is to create useful solutions within a set spending limit and deployment
+                     timeline.</p>
+
+                  <!-- <div class="about-btn">
+                     <a href="about.php" class="learn-btn">Learn More</a>
+                  </div> -->
+               </div>
+            </div>
+
+            <div class="tab-pane fade fcontent1" id="pills-contact" role="tabpanel"
+               aria-labelledby="pills-contact-tab">
+               <div class="overlay">
+                  <h3 class="main-head">DATA ENTRY AND ADMIN</h3>
+
+                  <p>Data entry jobs entail entering data into the organization's computer system for processing
+                     and administration from numerous sources. A candidate for a job in data entry will need to
+                     effectively manage a vast volume of data, most of it confidential or sensitive. The data that
+                     a firm generates determines its integrity, which supports the significance of the data entry
+                     role.
+
+                     Any data entered by the data entry team will subsequently be used by others for reports or as
+                     a source of reference.</p>
+                  <p>The primary destination will typically be listed in a job description for data entry:
+                     Document preparation and sorting for data entry the process of entering data into database
+                     software and confirming that it is accurate resolving informational inconsistencies and
+                     gathering additional data for deficient documents creating data backups as part of a backup
+                     strategy supplying information in response to requests from recognised “testing software
+                     updates and new database systems.</p>
+
+                     <!-- <div class="about-btn">
+                     <a href="about.php" class="learn-btn">Learn More</a>
+                  </div> -->
+               </div>
+
+            </div>
+
+         </div>
+
+      </div>
+
+   </div>
+
+</div>
+
+</section>
+
 
 <!-- ***************************inquiry SECTION****************************** -->
 
